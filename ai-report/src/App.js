4010e749cefd437df9f99b5432c0b779b7f9697c
Teresa Malave <dev.malave7@gmail.com>
2020-04-29 09:52:04 -0500

    pushing/making foundation

--- ai-report/src/App.js
+++ ai-report/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar';
+// import Intro from './components/Intro';
+// import Ethics from './components/Ethics';
+// import Governing from './components/Governing';
 
 function App() {
   return (
-    <div className="App">
-      
+
+    <div>
+      <Navbar />
+      <h1>main component</h1>
     </div>
+    
+
+    // <Router>
+    //   <Navbar />
+    //   <Route exact path ="/" component= {Intro} />
+    //   <Route exact path ="/ai-ethics" component= {Ethics} />
+    //   <Route exact path ="/ai-governing" component= {Governing} />
+    // </Router>
   );
 }
 
