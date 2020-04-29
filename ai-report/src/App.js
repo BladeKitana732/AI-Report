cbc9700619845411cfda6a83db60da3992a2860f
Teresa Malave <dev.malave7@gmail.com>
2020-04-29 09:54:05 -0500

    got foundation set

--- ai-report/src/App.js
+++ ai-report/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-// import Intro from './components/Intro';
-// import Ethics from './components/Ethics';
-// import Governing from './components/Governing';
+import Intro from './components/Intro';
+import Ethics from './components/Ethics';
+import Governing from './components/Governing';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
 
-    <div>
+    <Router>
       <Navbar />
-      <h1>main component</h1>
-    </div>
-    
-
-    // <Router>
-    //   <Navbar />
-    //   <Route exact path ="/" component= {Intro} />
-    //   <Route exact path ="/ai-ethics" component= {Ethics} />
-    //   <Route exact path ="/ai-governing" component= {Governing} />
-    // </Router>
+      <Route exact path ="/" component= {Intro} />
+      <Route exact path ="/ai-ethics" component= {Ethics} />
+      <Route exact path ="/ai-governing" component= {Governing} />
+    </Router>
   );
 }
 
