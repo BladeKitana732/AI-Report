2a8b1e2cf724b9d6bbbc154679e09999509f5f21
Teresa Malave <dev.malave7@gmail.com>
2020-05-01 18:39:13 -0500

    some styling

--- ai-report/src/components/Intro.js
+++ ai-report/src/components/Intro.js
@@ -5,7 +5,7 @@ export default function Intro() {
     return (
         <div>
             <h1>What is <em>Artificial Intelligence</em>?</h1>
-            <img src = { imageAI } alt= 'ai-intelligence'/>
+            <img src = { imageAI } alt= 'ai-intelligence' style = {{ width: 700, height: 200}}/>
             <p>Artificial Intelligence, also referred to as AI for short,involves using computers to do things that traditionally require human intelligence. Just like someone in the field of computer science or software engineering, those in this field also create algorithms to classify, analyze, and draw predictions from data.</p>
 
         </div>
