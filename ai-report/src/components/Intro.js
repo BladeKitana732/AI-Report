81aa70cc0ef57d1d937ef681c7b2bbfc5a1f5e3b
Teresa Malave <dev.malave7@gmail.com>
2020-05-01 18:59:30 -0500

    more info for section

--- ai-report/src/components/Intro.js
+++ ai-report/src/components/Intro.js
@@ -6,7 +6,9 @@ export default function Intro() {
         <div>
             <h1>What is <em>Artificial Intelligence</em>?</h1>
             <img src = { imageAI } alt= 'ai-intelligence' style = {{ width: 700, height: 200}}/>
-            <p>Artificial Intelligence, also referred to as AI for short,involves using computers to do things that traditionally require human intelligence. Just like someone in the field of computer science or software engineering, those in this field also create algorithms to classify, analyze, and draw predictions from data.</p>
+            <p>Artificial Intelligence, also referred to as AI for short,involves using computers to do things that traditionally require human intelligence.  Just like someone in the field of computer science or software engineering, those in this field also create algorithms to classify, analyze, and draw predictions from data.  Many believe that AI aims to put the human mind into a computer, although some researchers state it is their objective with their works, it mainly may be that they are using that phrase metaphorically due to the complexity of the brain I doubt anyone is serious about imitating them.  However, the collective objective based off my own research is that it aims to make computer programs that can solve problems and achieve goals in the world as well as humans.  How was AI first brought into the world?</p>
+
+            <p></p>
 
         </div>
     )
