bc9206b77f22bde6ffe25985ce5c46d3ba92f59a
Teresa Malave <dev.malave7@gmail.com>
2020-05-01 18:29:37 -0500

    imported image

--- ai-report/src/components/Intro.js
+++ ai-report/src/components/Intro.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import imageAI from './imageAI.jpg';
 
 export default function Intro() {
     return (
         <div>
             <h1>What is <em>Artificial Intelligence</em>?</h1>
+            <img src = { imageAI } alt= 'ai-intelligence'/>
             <p>Artificial Intelligence, also referred to as AI for short,involves using computers to do things that traditionally require human intelligence. Just like someone in the field of computer science or software engineering, those in this field also create algorithms to classify, analyze, and draw predictions from data.</p>
 
         </div>
