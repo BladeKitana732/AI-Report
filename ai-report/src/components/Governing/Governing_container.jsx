cc2ff5e2248618fb45b9bac8bf56bf4ead097ef5
Aleh <user@imac-user.myfiosgateway.com>
2020-05-03 02:58:54 -0400

    added styles

--- ai-report/src/components/Governing/Governing_container.jsx
+++ ai-report/src/components/Governing/Governing_container.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Player } from 'video-react';
-import ReactPlayer from "react-player"
+import ReactPlayer from "react-player";
+import "./styles/Governing_container.scss"
 
 export default function Governing_container(props) {
     return (
         <div className='Main'>
             <div className="Header">
-            <h2>{props.headText_top}</h2>
+                <h2>{props.headText_top}</h2>
                 <div className="firstBlock">
+                    <img src={props.article_Pic} alt="no img" width='600px'/>
                     <article>{props.article}</article>
-                    <img src={props.article_Pic} alt="no img" width='300px'/>
                     <article>{props.article_top_2}</article>
                     <article>{props.article_top_3}</article>
                     <article>{props.article_top_4}</article>
