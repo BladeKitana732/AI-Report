ba55872c13c8ba88b4ba4105f4ca88936fffb957
Aleh <user@imac-user.myfiosgateway.com>
2020-05-03 19:52:11 -0400

    finished container skeleton

--- ai-report/src/components/Ethics/Ethics_container.jsx
+++ ai-report/src/components/Ethics/Ethics_container.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './styles/Ethics_container.scss'
 
-export default function Ethics_container() {
+export default function Ethics_container(props) {
     return (
         <div>
-            <h2>im alive</h2>
+            <div className="first_block article_block">
+                <h2>{props.header_text}</h2>
+                <article>{props.article_top}</article>
+                <article>{props.article_top2}</article>
+            </div>
+            <div className="second_block article_block">
+                <h2>{props.header_middle}</h2>
+                <article>{props.article_middle}</article>
+                <article>{props.article_middle2}</article>
+                <article>{props.article_middle3}</article>
+            </div>
+            <div className="third_block article_block">
+                <h2>{props.header_middle2}</h2>
+                <article>{props.article_middle4}</article>
+                <article>{props.article_middle5}</article>
+            </div>
+            <div className="fourth_block article_block">
+                <h2>{props.header_bottom}</h2>
+                <article>{props.article_bottom}</article>
+                <article>{props.article_bottom2}</article>
+            </div>
+            <div className="fifth_block article_block">
+                <h2>{props.header_bottom2}</h2>
+                <article>{props.article_bottom3}</article>
+                <article>{props.article_bottom4}</article>
+            </div>
         </div>
     )
 }
