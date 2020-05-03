1f7d3be2e19015d1b950c01a36ee04ea8c98148e
Aleh <user@imac-user.myfiosgateway.com>
2020-05-03 00:57:38 -0400

    components structure finished

--- ai-report/src/components/Governing/Governing_container.jsx
+++ ai-report/src/components/Governing/Governing_container.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Player } from 'video-react';
+import ReactPlayer from "react-player"
 
 export default function Governing_container(props) {
     return (
         <div>
             <div className="Header">
-            <h2>{props.headText}</h2>
-            <article>{props.article}</article>
-            <img src={props.articlePic} alt="no img" width='500px'/>
-            <article>{props.article_2}</article>
-            <article>{props.article_3}</article>
-            <h2>123</h2>
+            <h2>{props.headText_top}</h2>
+                <div className="firstBlock">
+                    <article>{props.article}</article>
+                    <img src={props.article_Pic} alt="no img" width='300px'/>
+                    <article>{props.article_top_2}</article>
+                    <article>{props.article_top_3}</article>
+                    <article>{props.article_top_4}</article>
+                    <article>{props.article_top_5}</article>
+                </div>
+                <div className="secondBlock">
+                    <article>{props.article_middle}</article>
+                    <img src={props.article_Pic2} alt="no img" width='300px'/>
+                    <article>{props.article_middle2}</article>
+                    <article>{props.article_middle3}</article>
+                    <article>{props.article_middle4}</article>
+                    <article>{props.article_middle5}</article>
+                    <article>{props.article_middle6}</article>
+                    <article>{props.article_middle7}</article>
+                    <div className="videLink">
+                    <ReactPlayer
+                        url="https:www.youtube.com/watch?v=775DYqKA5Ks"
+                    />
+                    </div>
+                </div>
+                <div className="thirdBlock">
+                    <article>{props.article_bottom}</article>
+                    <img src={props.article_Pic3} alt="no img" width='300px'/>
+                    <article>{props.article_bottom2}</article>
+                    <article>{props.article_bottom3}</article>
+                    <article>{props.article_bottom4}</article>
+                    <div className="videLink">
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=oXEOXDS8nJ8"
+                    />
+                    </div>
+                </div>
+
             </div>
         </div>
     )
