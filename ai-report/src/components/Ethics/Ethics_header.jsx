c078e9a0866afb50bb2529057d460029d01ff313
Aleh <user@imac-user.myfiosgateway.com>
2020-05-03 15:49:45 -0400

    started to work on ethics

--- ai-report/src/components/Ethics/Ethics_header.jsx
+++ ai-report/src/components/Ethics/Ethics_header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './styles/Ethics_header.scss'
 
 
-export default function Ethics_header() {
+export default function Ethics_header(props) {
     return (
-        <div>
-            <h1>Im Ethics_header</h1>
+        <div className='Header__main'>
+            <div className="Header__main-container">
+                <h1>Im Ethics_header</h1>
+                <img src={props.image_path4} alt="no image to display" height='200'/>
+                <img src={props.image_path2} alt="no image to display" width='350' />
+   
+                <img src={props.image_path3} alt="no image to display" width='350'/>
+                <article>{props.statement}</article>
+                <article>{props.statement2}</article>
+            </div>
+            {/* <img src={props.image_path} alt="no image to display" /> */}
         </div>
     )
 }
