71c122dd7895bc87b831842d0045cd57e1b24fb9
Aleh <user@imac-user.myfiosgateway.com>
2020-05-03 18:51:27 -0400

    finished with header

--- ai-report/src/components/Ethics.js
+++ ai-report/src/components/Ethics.js
@@ -8,11 +8,11 @@ export default function Ethics() {
                 image_path="https://www.telefonica.de/file/repository/36993_t_1024_576/2/0/KI-Prinzipien-englisch-1920x1080.jpg"
                 image_path2="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTPmhRz7jzAubyyxiFBJwe29g63U4R0GO9LrIxmhRJOi2RBdt4I&usqp=CAU"
                 image_path3="https://opendatascience.com/wp-content/uploads/2019/06/Screen-Shot-2019-06-26-at-8.48.52-AM-750x350.png"
-                image_path4="https://www.governmentciomedia.com/sites/default/files/styles/featured_article_image/public/2019-03/Screen%20Shot%202019-03-11%20at%201.49.45%20PM.png?itok=QMMmxNPM"
+                image_path4="https://www.planbox.com/wp-content/uploads/2017/07/AdobeStock_1424583961.jpg"
                 statement='
-                    In other words, the ethics of AI lies in the ethical quality of its prediction, the ethical quality of the end outcomes drawn out of that and the ethical quality of the impact it has on humans. Ethics is also defined as: The discipline dealing with right vs wrong, and the moral obligations and duties of humans.'
+                    " In other words, the ethics of AI lies in the ethical quality of its prediction, the ethical quality of the end outcomes drawn out of that and the ethical quality of the impact it has on humans. Ethics is also defined as: The discipline dealing with right vs wrong, and the moral obligations and duties of humans."'
                 statement2='
-                    The term "robot ethics" (sometimes "roboethics") refers to the morality of how humans design, construct, use and treat robots and other artificially intelligent beings.It considers both how artificially intelligent beings may be used to harm humans and how they may be used to benefit humans.'
+                    " The term "robot ethics" (sometimes "roboethics") refers to the morality of how humans design, construct, use and treat robots and other artificially intelligent beings.It considers both how artificially intelligent beings may be used to harm humans and how they may be used to benefit humans."'
             />
         </div>
     )
--- ai-report/src/components/Ethics/Ethics_header.jsx
+++ ai-report/src/components/Ethics/Ethics_header.jsx
@@ -6,13 +6,16 @@ export default function Ethics_header(props) {
     return (
         <div className='Header__main'>
             <div className="Header__main-container">
-                <h1>Im Ethics_header</h1>
-                <img src={props.image_path4} alt="no image to display" height='200'/>
-                <img src={props.image_path2} alt="no image to display" width='350' />
+                <h1>The Ethics of Artificial Intelligence</h1>
+                <img src={props.image_path4} alt="no image to display"/>
+                <img src={props.image_path2} alt="no image to display" width='450' />
    
-                <img src={props.image_path3} alt="no image to display" width='350'/>
-                <article>{props.statement}</article>
-                <article>{props.statement2}</article>
+                <img src={props.image_path3} alt="no image to display" width='450'/>
+                <div className="Header__main-container-article">
+                    <article>{props.statement}</article>
+                    <article>{props.statement2}</article>
+                </div>
+
             </div>
             {/* <img src={props.image_path} alt="no image to display" /> */}
         </div>
